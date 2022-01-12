4d2d343f10f6dd8fdaea2b270082ce159f84b56d
ken <kankinken9@gmail.com>
2022-01-12 22:41:51 +0800

    Initial Commit

---
+++ MiniBus/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MiniBus
+//
+//  Created by kenkan on 12/1/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
